e4f588fc734eeb8a806e0c41d8093af7598d36ff
Nadeem Ahmed <nadeem.a9891@gmail.com>
2023-08-30 20:07:19 +0530

    JavaScript

---
+++ JavaScript/index.js
@@ -0,0 +1,12 @@
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+
+// YOUR CODE BELOW THIS LINE:
+
+let whisper = message.trim()
+
+whisper = whisper.toLowerCase()
+
+
+
+
