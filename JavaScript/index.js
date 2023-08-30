2dd51c58e5f0f98f2e5749c484cc07c3d44e3dc2
Nadeem Ahmed <nadeem.a9891@gmail.com>
2023-08-30 21:25:29 +0530

    JavaScript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,12 +1,25 @@
-// DON'T TOUCH THIS LINE!
-const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+// // DON'T TOUCH THIS LINE!
+// const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
 
-// YOUR CODE BELOW THIS LINE:
+// // YOUR CODE BELOW THIS LINE:
 
-let whisper = message.trim()
+// let whisper = message.trim()
 
-whisper = whisper.toLowerCase()
+// whisper = whisper.toLowerCase()
+
+// code giving multiple other methods to light
+
+// ===================
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+let box = word.slice(5);
+
+const facialHair = box.replace("o", "e");
+
+console.log(facialHair);
 
 
+// let facialHair = word.slice(5).replace("o", "e");
 
 
