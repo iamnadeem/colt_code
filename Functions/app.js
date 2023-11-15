02bf2e756e55c21a82d752abb83c7ea9e40e17fc
Nadeem Ahmed <nadeem.a9891@gmail.com>
2023-11-15 13:46:44 +0530

    Functions

--- Functions/app.js
+++ Functions/app.js
@@ -16,13 +16,36 @@ printHeart();
 console.log('*************');
 
 
-function rant(message){
+// function rant(message){
 
-    for (let i=0; i<3;i++){
-        console.log(message.toUpperCase());
+//     for (let i=0; i<3;i++){
+//         console.log(message.toUpperCase());
+//     }
+// }
+//     // console.log(message.toUpperCase());
+//     // console.log(message.toUpperCase());
+
+// rant("i hate beets");
+
+
+// function isSnakeEyes(roll1, roll2){
+//     if ((roll1===1) && (roll2===1)){
+//         console.log('Snake Eyes!');
+//     }
+//     else{
+//         console.log('Not Snake Eyes');
+//     }
+// }
+// isSnakeEyes(2,2);
+function isSnakeEyes(die1, die2) {
+    if (die1 === 1 && die2 === 1) {
+      console.log('Snake Eyes!');
+    } else {
+      console.log('Not Snake Eyes!');
     }
-}
-    // console.log(message.toUpperCase());
-    // console.log(message.toUpperCase());
+  }
 
-rant("i hate beets");
+  // DEFINE YOUR FUNCTION BELOW:
+function multiply(num1, num2){
+    return num1 * num2;
+}
