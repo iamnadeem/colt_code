f4df8bc99145a42cc73c0085837cc0769c8cf659
Nadeem Ahmed <nadeem.a9891@gmail.com>
2023-11-25 18:09:06 +0530

    Array map

--- Functions/app.js
+++ Functions/app.js
@@ -210,3 +210,13 @@ let hen={
   }
       
   }
+
+// DO NOT ALTER THE FOLLOWING CODE: using MAP to make a new array out of it.
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+// Write your code here
+
+const firstNames = fullNames.map(function (fullNames){
+    return fullNames.first;
+})
+
